d4aeeae43bd60b3d9d7e30f3d0a8b9591cddecda
Ruben Mayer <rubenmayer@gmail.com>
2016-06-20 17:41:00 -0700

    adding html response

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  app.use('/static', express.static('indexx'))
-  response.send('Hello00 World!')
-})
+    res.sendFile('static/index.html');
+});
+
+
+  //app.use('/static', express.static('indexx'))
+  //response.send('Hello00 World!')
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
