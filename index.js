92f2bf1a9f35136c9ed51a027d9b0dc034606f87
Ruben Mayer <rubenmayer@gmail.com>
2016-06-20 17:36:32 -0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express')
+var fs = require("fs");
+var path = require("path");
+
+
+
+
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  app.use('/static', express.static('index'))
+  response.send('Hello00 World!')
 })
 
 app.listen(app.get('port'), function() {
