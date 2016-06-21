dd856b1f3932e92cabff00b158a4e6362ed5c8cc
Ruben Mayer <rubenmayer@gmail.com>
2016-06-21 11:38:47 -0700

    pushing

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express')
 var fs = require("fs");
 var path = require("path");
+var firebase = require('firebase');
+var nodemailer = require('nodemailer');
+var schedule = require('node-schedule');
+var Promise = require('promise');
+var escape = require('escape-html');
+
+var messageCount = 0
 
 
 
@@ -15,6 +22,11 @@ app.get('/', function(request, response) {
 });
 
 
+firebase.initializeApp({
+  databaseURL: 'https://crowdamp-messaging.firebaseio.com',
+  serviceAccount: path.join(__dirname + '/CrowdAmpMessaging-5e5474ce420c.json')
+});
+
   //app.use('/static', express.static('indexx'))
   //response.send('Hello00 World!')
 
@@ -22,3 +34,62 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+
+
+function startListeners() {
+  firebase.database().ref("/MessageData").on('child_added', function(snapshot) {
+    var snapshotPath = '/MessageData' + '/' + snapshot.key
+    console.log(snapshotPath)
+		firebase.database().ref(snapshotPath).on('child_added', function(snapshot) {
+    		
+			//sendMessageToUser(snapshotPath ,snapshot.key, snapshot.child('text').val(), 'text')
+    		console.log(snapshot.child("text").val())
+		})
+  });
+  console.log("starting listener")
+}
+
+
+function sendMessageToUser(snapshotPath,userId, messageContent, messageType) {
+  messageRef = firebase.database().ref(snapshotPath).push()
+	var messageItem = {
+            "text": messageContent,
+            "senderId": userId,
+            "sentByUser": false,
+            "type": "text",
+            "fileName": ""
+        }
+    messageRef.set(messageItem)
+}
+
+var onesignal = require('node-opensignal-api');
+var onesignal_client = onesignal.createClient();
+ 
+
+function sendNotification() { 
+  var restApiKey = 'N2Y2MWU1MDMtOTk3Zi00MDkzLWI3NjEtYTU0N2UwYjFjMGRh';
+  var params = {
+    app_id: '3fe58d49-2025-4653-912f-8067adbecd7f',
+    contents: {
+      'en': 'Notification from Online!'
+    },
+    include_player_ids: ["8e70c1e0-d3ce-43a7-8a69-79477762bf33"],
+    isIos: true
+  };
+  onesignal_client.notifications.create(restApiKey, params, function (err, response) {
+    if (err) {
+        console.log('Encountered error', err);
+      } else {
+        console.log(response);
+      }
+  });
+}
+
+
+
+
+startListeners();
+//sendMessageToUser("/MessageData/mgOVbPwSaPNxAskRztKFGZoTSqz1","-KKlIa_WDOmwDyloSPPD","heyyyyy", "text")
+sendNotification()
+
