a4a988aede24c7e07e047f4c4ff91709c67733f9
Ruben Mayer <rubenmayer@gmail.com>
2016-06-20 17:47:45 -0700

    asdf

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-    response.sendFile((path.join(__dirname + '/static/index.html'));
+    response.sendFile((path.join(__dirname + '/static/index.html')));
 });
 
 
