451c0ecd7142a50ed50a3aa1b4e1b76be54cb579
Ruben Mayer <rubenmayer@gmail.com>
2016-07-14 11:17:59 -0700

    shell

--- index.js
+++ index.js
@@ -623,7 +623,9 @@ function sendIntroFlow(req, phoneNumberToSendFrom) {
     //}, 10000);
     //forwardMessageFromServerToUsers('electionfails', 'magnus', 'text', firebasePath, req.body.From, '')
 
-  } else {
+  } else if (req.body.To == '+18608214181'){ 
+    sendMessageThroughTwilio(req.body.From, phoneNumberToSendFrom, "Hey! this is Kyle, thanks for messaging me! I will try to answer messages as soon as I can :) Also, I can send you updates about what I'm up to. Sound cool?", "")
+  }
 
     sendMessageThroughTwilio(req.body.From, req.body.To, "Hey B, thanks for messaging me this is ArianaBot <3", "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSmeJSGr2yKOGUJF514n6isuRTjuvySdbyJD1lDKI0U3TMiAf-OHQ")
     sendMessageThroughTwilio(req.body.From, req.body.To, "Hey B, thanks for messaging me this is ArianaBot <3. I talk and act like Ariana Grande", "")
