93bea05c60be9f9ce3608834aec843308dff74cf
Ruben Mayer <rubenmayer@gmail.com>
2016-07-14 11:26:56 -0700

    shell

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ var phoneNumberToInfluencerIdDict = {
   '+13343778787' : 'morggkatherinee',
   '+18313378787' : 'morggkatherinee',
   '+16468768787' : 'morggkatherinee',
-  '+18608214181' : 'kyleexum'
+  '+18608214181' : 'kyleexum',
+  '+18674214179' : 'kyleexum'
 
 
 
@@ -587,7 +588,7 @@ app.post('/twiliowebhook/', function (req, res) {
             addItemToFirebaseDatabase(phoneNumberToInfluencerIdDict[req.body.To] + "/IndividualMessageData/" +  req.body.From, "isUsingApp", false)
             addItemToFirebaseDatabase(phoneNumberToInfluencerIdDict[req.body.To] + "/IndividualMessageData/" +  req.body.From, undefined, messageItemDict)
 
-            sendMessageThroughTwilio(req.body.From, req.body.To, "You've reached " + phoneNumberToInfluencerIdDict[req.body.to]  + "! This is an automatic message to let you know know that you can text me directly at: " + phoneNumberToSendFrom + " The purpose of this message is to filter any SPAM that I would otherwise recieve.", "")
+            sendMessageThroughTwilio(req.body.From, req.body.To, "You've reached " + influencerIdToNameDict[phoneNumberToInfluencerIdDict[req.body.To]]  + "! This is an automatic message to let you know know that you can text me directly at: " + phoneNumberToSendFrom + " The purpose of this message is to filter any SPAM that I would otherwise recieve.", "")
             setTimeout(function() {
               sendIntroFlow(req, phoneNumberToSendFrom)
 // sendMessageThroughTwilio(req.body.From, phoneNumberToSendFrom, "Hey! this is " + influencerIdToNameDict[influencerId] + " again :)", "")
