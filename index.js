ee2b7984525003fdd3846e61d8cd5278cf70c8dd
Ruben Mayer <rubenmayer@gmail.com>
2016-07-20 15:25:34 -0700

    shell

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ var phoneNumberToInfluencerIdDict = {
   '+18603081572' : 'kyleexum',
   '+18603081503' : 'kyleexum',
   '+12568134679' : 'jvrionis',
-  '+12562026082' : 'jvrionis'
+  '+12562026194' : 'jvrionis'
 
 
 
@@ -753,7 +753,7 @@ function phoneNumberFormatter(phoneNumber) {
 }
 
 function sendIntroFlow(req, phoneNumberToSendFrom) {
-  if (req.body.to == '+12568134679') {
+  if (req.body.to == '+12562026194') {
     sendMessageThroughTwilio(req.body.From, phoneNumberToSendFrom, "Hey! this is John, thanks for messaging me! I will try to answer messages as soon as I can :) Also, I can send you updates about what I'm up to. Sound cool?", "")
   } else if (req.body.To == '+16506678787') {
     sendMessageThroughTwilio(req.body.From, phoneNumberToSendFrom, "Hey! this is Morgan, thanks for messaging me! I will try to answer messages as soon as I can :) Also, I can send you updates about what I'm up to. Sound cool?", "")
