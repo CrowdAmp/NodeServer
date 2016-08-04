32c1ab5bf6432ac8fa4c089399cc1e23add03539
Ruben Mayer <rubenmayer@gmail.com>
2016-08-04 11:41:48 -0700

    shell

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ app.get('/getTotalFans/:id', function(request, response) {
 
 app.get('/didShare/:id', function(request, response) {
   console.log("GET TOTAL FANS: " + influencerMetricsDict[request.params.id])
-  response.send(0)
+  response.send("0")
 })
 
 app.get('/updateTwitterAuthorization/:influencerid/:id/:status', function(request, response) {
