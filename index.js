0e73d5486a6476934927f9b6fb6432340ef4c609
Ruben Mayer <rubenmayer@gmail.com>
2016-08-04 08:57:49 -0700

    shell

--- index.js
+++ index.js
@@ -756,7 +756,7 @@ function listenForNewUserUpdates(platform) {
     var userId = snapshot.key
     if (snapshot.child("hasRecorded").val() == null || snapshot.child("hasRecorded").val() == true) {
       console.log("LISTENING FOR New user UPDATES")
-      addItemToFirebaseDatabase('trumpbot/' + platform + '/' + userId, "hasRecorded", false)
+      addItemToFirebaseDatabase('trumpbot/' + platform + '/' + userId, "hasRecorded", "shouldBeTrue")
       forwardMessageFromServerToUsers("trumpbot", "Hi I am Trump Bot I am going to MAKE TEXTING GREAT AGAIN!", "text", "trumpbot/IndividualMessageData/", userId, "") 
       forwardMessageFromServerToUsers("trumpbot", "It might take me a few minutes to reply, but I will definitely get back to you!", "text", "trumpbot/IndividualMessageData/", userId, "") 
       forwardMessageFromServerToUsers("trumpbot", "If at any time you want to Tweet or share a screenshot of this conversation on Facebook, simply reply SHARE", "text", "trumpbot/IndividualMessageData/", userId, "") 
@@ -768,7 +768,7 @@ function listenForNewUserUpdates(platform) {
     var userId = snapshot.key
     if (snapshot.child("hasRecorded").val() == null || snapshot.child("hasRecorded").val() == true) {
       console.log("LISTENING FOR New user UPDATES")
-      addItemToFirebaseDatabase('indibot/' + platform + '/' + userId, "hasRecorded", false)
+      addItemToFirebaseDatabase('indibot/' + platform + '/' + userId, "hasRecorded", "shouldBeTrue")
       forwardMessageFromServerToUsers("indibot", "Hi I am Indi Bot I will be your fitness companion!", "text", "indibot/IndividualMessageData/", userId, "") 
       forwardMessageFromServerToUsers("indibot", "It might take me a few minutes to reply, but I will definitely get back to you!", "text", "indibot/IndividualMessageData/", userId, "") 
       forwardMessageFromServerToUsers("indibot", "If at any time you want to Tweet or share a screenshot of this conversation on Facebook, simply reply SHARE", "text", "indibot/IndividualMessageData/", userId, "") 
