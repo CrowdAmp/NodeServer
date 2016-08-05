f0853bbef1b0124cf074e1c05dd1b6d867a3a374
Ruben Mayer <rubenmayer@gmail.com>
2016-08-04 17:00:35 -0700

    shell

--- index.js
+++ index.js
@@ -702,7 +702,7 @@ function listenForNewMessages() {
           }
           if (userContactInfo && !snapshot.child('hasBeenForwarded').val()) {
             addItemToFirebaseDatabase('/' + influencerId + '/IndividualMessageData/' + snapshot.child("senderId").val() + "/" + snapshot.key, "hasBeenForwarded", true)
-            sendPushNotification([pushNotificationDict[snapshot.child("senderId").val()][0]], [pushNotificationDict[snapshot.child("senderId").val()][1]],"Message from " + numberOfUsers + " fans: " + content)
+            sendPushNotification([pushNotificationDict[snapshot.child("senderId").val()][0]], [pushNotificationDict[snapshot.child("senderId").val()][1]], getPushNotificationMessage(snapshot.child("senderId").val()))
  
           }
           if (snapshot.child('text').val() != null) {
